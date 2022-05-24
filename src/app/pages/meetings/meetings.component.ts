a2f934144a62791bab7c2ffdaba16715421871cd
Sean Allen <sean.allen@humboldtcode.club>
2022-05-24 13:35:09 -0700

    feat: Club Meetings
    Add content to the club meetings page.

--- src/app/pages/meetings/meetings.component.ts
+++ src/app/pages/meetings/meetings.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'csc-meetings',
-  templateUrl: './meetings.component.html'
+  templateUrl: './meetings.component.html',
+  styleUrls: ['./meetings.component.scss']
 })
 export class MeetingsComponent { }
